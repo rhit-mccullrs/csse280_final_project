1a9959ccf6d1c98e6082ba60bd3401b512cf289d
LukeVanScoyoc <vanscolm@rose-hulman.edu>
2025-11-02 15:26:53 -0500

    test 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './lists.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root-index')).render(
@@ -9,8 +10,8 @@ createRoot(document.getElementById('root-index')).render(
   </StrictMode>,
 )
 
-createRoot(document.getElementById('root-lists')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root-lists')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
